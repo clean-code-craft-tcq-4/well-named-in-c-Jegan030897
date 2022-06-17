ef2cce5cf967e8fa14fbbcba392e262bc6e33287
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-06-17 09:07:28 +0530

    Update getCable_ColorCode.c

--- getCable_ColorCode.c
+++ getCable_ColorCode.c
@@ -22,24 +22,3 @@ int GetPairNumberFromColorCode(const ColorPair* colorCode) {
     return colorCode->majorColorCode * numberOfMinorColors + colorCode->minorColorCode + 1;
 }
 
-void PrintColorCodeReferenceManual(void)
-{
-    int majorColorCode_Index;
-    int minorColorCode_Index;
-    int pairNumber = 1;
-    
-    printf("-----------------------------------\n");
-    printf("25-pair color code reference manual\n");
-    printf("-----------------------------------\n");
-    printf("| Pair no.| Major color | Minor color |\n");
-    printf("-----------------------------------\n");
-    for(majorColorCode_Index = 0; majorColorCode_Index < numberOfMajorColors; majorColorCode_Index++)
-    {
-       for(minorColorCode_Index = 0; minorColorCode_Index < numberOfMinorColors; minorColorCode_Index++) 
-       {
-           printf("| %d | %s | %s |\n",pairNumber++, MajorColorNames[majorColorCode_Index], MinorColorNames[minorColorCode_Index]);
-       }
-        printf("-----------------------------------\n");
-    }
-    printf("-----------------------------------\n");
-}
